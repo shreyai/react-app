fef6a18ef849a24dee4b17e0d538400aa92b7eb2
Shreya Vishwakarma <shreya@appsimpact.com>
2020-08-22 16:50:04 +0530

    defined a component with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Professional from './Professional/Professional';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React Component</h1>
+      <Professional name="Ravi Singh" profession="Software Engineer" />
+      <Professional name="Monu Singh" profession="Mechanical Engineer" />
+      <Professional name="Arijit Singh" profession="Musician" />
     </div>
   );
 }
---
+++ src/Professional/Professional.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const professional = (props) => {
+    return <p>I am {props.name} and I am a {props.profession} by profession.</p>
+}
+
+export default professional;
