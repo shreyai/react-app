9cbf4956e0da8e4bc63d65cfa6ee668359c0e4f9
Shreya Vishwakarma <shreya@appsimpact.com>
2020-08-22 17:19:48 +0530

    user state in app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Professional from './Professional/Professional';
 
 
 function App() {
+
+  const [ professionalState, setProfessionalState ] = useState({
+    professionals : [
+      { name: 'Ravi Singh', profession: 'Software Engineer' },
+      { name: 'Mony Singh', profession: 'Mechanical Engineer' },
+      { name: 'Arijit Singh', profession: 'Musician' }
+    ]
+  })
+
   return (
     <div className="App">
       <h1>Hi, I'm a React Component</h1>
-      <Professional name="Ravi Singh" profession="Software Engineer" />
-      <Professional name="Monu Singh" profession="Mechanical Engineer" />
-      <Professional name="Arijit Singh" profession="Musician" />
+      <Professional name={professionalState.professionals[0].name} profession={professionalState.professionals[0].profession} />
+      <Professional name={professionalState.professionals[1].name} profession={professionalState.professionals[1].profession} />
+      <Professional name={professionalState.professionals[2].name} profession={professionalState.professionals[2].profession} />
     </div>
   );
 }
